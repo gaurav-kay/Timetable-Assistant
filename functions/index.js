80e124dbd9bdddafa13feda143c175007bfa34e6
Gaurav Kashyap <gauravandkashyap@gmail.com>
2019-10-31 16:05:55 +0530

    updated sign in

--- functions/index.js
+++ functions/index.js
@@ -4,56 +4,64 @@ const {
       Table,
       Image,
       Button,
-      SignIn
+      SignIn,
+      Suggestions
 } = require('actions-on-google')
 const admin = require('firebase-admin');
 const functions = require('firebase-functions')
 const firebase = require('firebase/app')
+const config = require('./config')
 
-const firebaseConfig = {
-      apiKey: "AIzaSyC7TDSiX_JQV7XwXe5kNSizASfdKehkuE4",
-      authDomain: "timetable-7b63e.firebaseapp.com",
-      databaseURL: "https://timetable-7b63e.firebaseio.com",
-      projectId: "timetable-7b63e",
-      storageBucket: "timetable-7b63e.appspot.com",
-      messagingSenderId: "848995382512",
-      appId: "1:848995382512:web:cbf056ea923c505b"
-};
-
-firebase.initializeApp(firebaseConfig);
-admin.initializeApp(functions.config().firebase);
-var db = admin.firestore();
+const firebaseConfig = config.default.firebaseConfig
+firebase.initializeApp(firebaseConfig)
+admin.initializeApp(functions.config().firebase)
+var db = admin.firestore()
 
 const app = dialogflow({
       debug: true,
-      clientId: "848995382512-p422t8evvlp9th9oid2ofcrim2lkp3kq.apps.googleusercontent.com"
+      clientId: config.default.clientId
 })
 
 app.intent('Default Welcome Intent', (conv) => {
-      conv.ask(new SignIn('To get your Timetable'))
+      const payload = conv.user.profile.payload
+      if (!payload.sub) {
+            return conv.ask(new SignIn('To get your Timetable'))
+      } else {
+            respondWithTimetable(conv)
+      }
+})
 
-      var hongKong = new Date()
-      var currentIndiaTime = hongKong
-      currentIndiaTime.setHours(hongKong.getHours() - 2, hongKong.getMinutes() - 30)
-      currentIndiaTime = currentIndiaTime.toString()
-      var day = currentIndiaTime.split(' ')[0].toLowerCase()
+function respondWithTimetable(conv) {
+      const payload = conv.user.profile.payload
 
-      var holidayStatus = checkHoliday(currentIndiaTime)
-      if (!holidayStatus.isHoliday) {
-            var timetable = getTimetable(day)
+      db.collection('users').doc(payload.sub).get()
+            .then((userDocSnapshot) => {
+                  db.collection('timetables').doc(userDocSnapshot.data().class).get()
+                        .then((timetableDocSnapshot) => {
+                              sendResponse(timetableDocSnapshot.data())
+                        })
+
+            })
+
+
+      function sendResponse(docData) {
+            let hongKong = new Date()
+            let currentIndiaTime = hongKong
+            currentIndiaTime = currentIndiaTime.setHours(hongKong.getHours() - 2, hongKong.getMinutes() - 30).toString()
+            let day = currentIndiaTime.split(' ')[0].toLowerCase()
+
+            const timetable = docData[day].timetable  // docdata[day] must contain timetable and day in full form
 
             conv.close(new SimpleResponse({
                   text: "Today's time table is",
-                  // speech: `You have these classes on ${timetable.day}`
                   speech: getSpeech(timetable, currentIndiaTime)
-                  // TODO text/speech should tell books to be swapped
             }))
             conv.close(new Table({
-                  title: `${timetable.day}'s classes`,
+                  title: `${docData[day].day}'s classes`,
                   subtitle: `Classes you have today`,
                   image: new Image({
                         url: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/5a/Ramaiah_Institutions_Logo.png/220px-Ramaiah_Institutions_Logo.png',
-                        alt: 'Test alt text and image'
+                        alt: 'Ramaiah Institute of Technology logo'
                   }),
                   columns: [
                         {
@@ -64,20 +72,55 @@ app.intent('Default Welcome Intent', (conv) => {
                         }
                   ],
                   rows: getRows(timetable),
-                  buttons: new Button({
-                        // TODO: add section changing option
-                        title: 'Change section',
-                        url: 'https://www.google.com' // test url
+            }))
+            conv.close(new Suggestions(['Change Class'], ['Send Feedback']))
+
+            function getRows(timetable) {
+                  var rowElements = []
+
+                  // by format specified in https://developers.google.com/actions/assistant/responses#table_cards
+                  timetable.periods.forEach((period, hourIndex) => {
+                        rowElements.push({
+                              cells: [period, timetable.periodTimes[hourIndex]],
+                              dividerAfter: false
+                        })
                   })
-            }))
+
+                  // Break hours indication by adding rulers
+                  if (rowElements.length >= 1 && rowElements.length < 4) {
+                        rowElements[1].dividerAfter = true
+                  } else if (rowElements.length >= 4) {
+                        rowElements[1].dividerAfter = true
+                        rowElements[3].dividerAfter = true
+                  }
+
+                  return rowElements
+            }
+            // TODO: update getspeech
+            function getSpeech(todayTimetable, todayDate) {
+                  var x = new Date(todayDate)
+                  var prevDate = (new Date(x)).setDate(x.getDate() - 1)
+
+                  if (prevDate.toString().split(' ')[0].toLowerCase() === 'sun') {
+                        prevDate.setDate(x.getDate() - 3)
+                  }
+
+                  var prevTimetable = getTimetable(prevDate.toString().split(' ')[0].toLowerCase())
+
+                  // https://stackoverflow.com/questions/1723168/what-is-the-fastest-or-most-elegant-way-to-compute-a-set-difference-using-javasc
+                  var removeBooks = prevTimetable.periods.filter(period => !todayTimetable.periods.includes(period))
+                  var addBooks = todayTimetable.periods.filter(period => !prevTimetable.periods.includes(period))
+
+                  if (removeBooks.length === 0) {
+                        return `Add ${addBooks.join(", ")}`
+                  } else if (addBooks.length === 0) {
+                        return `Remove ${removeBooks.join(", ")}`
+                  } else {
+                        return `Remove ${removeBooks.join(", ")} and add ${addBooks.join(", ")}`
+                  }
+            }
       }
-      else {
-            conv.ask(new SimpleResponse({
-                  text: `Today is holiday due to ${holidayStatus.reason}, Want to know tomorrow's timetable?`,
-                  speech: "You don't have classes today"
-            }))
-      }
-})
+}
 
 app.intent('Sign In Confirmation', async (conv, params, signin) => {
       if (signin.status !== 'OK') {
@@ -89,3 +132,7 @@ app.intent('Sign In Confirmation', async (conv, params, signin) => {
 exports.fulfillmentHKG = functions
       .region('asia-east2')
       .https.onRequest(app)
+
+// get class from suggestion chips
+
+// o/p from suggestion chip is intent text
